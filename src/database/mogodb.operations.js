bf3020ba2ad21b5ca874fd6ba212bd17159cd506
Happy Singh <hsingh0805@gmail.com>
2022-07-30 18:03:16 +0530

    Add mongodb CURD queries

--- src/database/mogodb.operations.js
+++ src/database/mogodb.operations.js
@@ -1,6 +1,7 @@
 const { mongoClient } = require("./mongodb.client");
 const logger = require("../logger/LoggerConfiguration");
 
+//INSERT Query
 async function insertRecordWithId(id, dbName, collectionName, jsonData) {
     let isInserted = false;
     try {
@@ -14,11 +15,39 @@ async function insertRecordWithId(id, dbName, collectionName, jsonData) {
             logger.info(`Failed to insert Document in ${collectionName} with id ${result.insertedId}`);
         }
     } catch(e) {
-        logger.error();
+        logger.error(e);
     }
 }
 
-async function getRecordById(dbName, collectionName, fieldName, fieldValue) {
+async function insertRecord(dbName, collectionName, jsonData) {
+    let isInserted = false;
+    try {
+        const result = await mongoClient.db(dbName).collection(collectionName).insertOne(jsonData); 
+        if(result.acknowledged){
+            isInserted = true;
+            logger.info(`Document inserted successfully in ${collectionName} with id ${result.insertedId}`);
+        }
+        else {
+            logger.info(`Failed to insert Document in ${collectionName} with id ${result.insertedId}`);
+        }
+    } catch(e) {
+        logger.error(e);
+    }
+}
+
+async function insertMultipleRecords(dbName, collectionName, jsonData) { //insert list of records
+    try {
+        const result = await mongoClient.db(dbName).collection(collectionName).insertMany(jsonData); 
+
+        logger.info(`List of Documents inserted successfully in ${collectionName} with id ${result.insertedId}`);
+    } catch(e) {
+        logger.error(e);
+    }
+}
+
+
+//RETRIEVE Query
+async function getSingleRecord(dbName, collectionName, fieldName, fieldValue) {
     let document;
     try {
         document = await mongoClient.db(dbName).collection(collectionName).findOne( {[fieldName]: fieldValue} ); 
@@ -34,6 +63,125 @@ async function getRecordById(dbName, collectionName, fieldName, fieldValue) {
     return document;
 }
 
+async function getRecordById(id, dbName, collectionName) {
+    let document;
+    try {
+        document = await mongoClient.db(dbName).collection(collectionName).findOne( {_id: id} ); 
+        if(document){
+            logger.info(`Document retrieved successfully`);
+        }
+        else {
+            logger.info(`Failed to retrieve document`);
+        }
+    } catch(e) {
+        logger.error(e);
+    }
+    return document;
+}
+
+async function getAllRecords(dbName, collectionName) {
+    let documentArr;
+    try {
+        documentArr = await mongoClient.db(dbName).collection(collectionName).find().toArray(); 
+        if(documentArr.length > 0){
+            logger.info(`Document retrieved successfully`);
+        }
+        else {
+            logger.info(`Failed to retrieve document`);
+        }
+    } catch(e) {
+        logger.error(e);
+    }
+    return documentArr;
+}
+
+async function getSingleRecordWithMultipleFilters(dbName, collectionName, filterJson) {
+    let document;
+    try {
+        document = await mongoClient.db(dbName).collection(collectionName).findOne(filterJson); 
+        if(document){
+            logger.info(`Document retrieved successfully`);
+        }
+        else {
+            logger.info(`Failed to retrieve document`);
+        }
+    } catch(e) {
+        logger.error(e);
+    }
+    return document;
+}
+
+async function getRecordsWithOffsetLimit(dbName, collectionName, fieldName, fieldValue, sortedFieldName, offset, limit) {
+    let documentArr;
+    try {
+        documentArr = await mongoClient.db(dbName).collection(collectionName).find({[fieldName]: fieldValue}).sort({ [sortedFieldName]: -1 })
+        .skip(offset).limit(limit).toArray(); 
+        if(documentArr.length > 0){
+            logger.info(`Document retrieved successfully`);
+        }
+        else {
+            logger.info(`Failed to retrieve document`);
+        }
+    } catch(e) {
+        logger.error(e);
+    }
+    return documentArr;
+}
+
+async function getRecordsWithProjectionOffsetLimit(dbName, collectionName, fieldName, fieldValue, projectionJson, sortedFieldName, offset, limit) {
+    let documentArr;
+    try {
+        documentArr = await mongoClient.db(dbName).collection(collectionName).find({[fieldName]: fieldValue}).project(projectionJson).sort({ [sortedFieldName]: -1 })
+        .skip(offset).limit(limit).toArray(); 
+        if(documentArr.length > 0){
+            logger.info(`Document retrieved successfully`);
+        }
+        else {
+            logger.info(`Failed to retrieve document`);
+        }
+    } catch(e) {
+        logger.error(e);
+    }
+    return documentArr;
+}
+
+async function getRecordsWithMultipleFilterProjectionOffsetLimit(dbName, collectionName, filterJson, projectionJson, sortedFieldName, offset, limit) {
+    let documentArr;
+    try {
+        documentArr = await mongoClient.db(dbName).collection(collectionName).find(filterJson).project(projectionJson).sort({ [sortedFieldName]: -1 })
+        .skip(offset).limit(limit).toArray(); 
+        if(documentArr.length > 0){
+            logger.info(`Document retrieved successfully`);
+        }
+        else {
+            logger.info(`Failed to retrieve document`);
+        }
+    } catch(e) {
+        logger.error(e);
+    }
+    return documentArr;
+}
+
+async function getAllRecordsWithOffsetLimit(dbName, collectionName, sortedFieldName, offset, limit) {
+    let documentArr;
+    try {
+        documentArr = await mongoClient.db(dbName).collection(collectionName).find().sort({ [sortedFieldName]: -1 })
+        .skip(offset).limit(limit).toArray(); 
+        if(documentArr.length > 0){
+            logger.info(`Document retrieved successfully`);
+        }
+        else {
+            logger.info(`Failed to retrieve document`);
+        }
+    } catch(e) {
+        logger.error(e);
+    }
+    return documentArr;
+}
+
+
+
+// UPDATE Queries
 async function updateFieldForRecord(dbName, collectionName, oldFieldName, oldFieldValue, newFieldName, newFieldValue) {
     let result;
     let isUpdated = false;
@@ -70,9 +218,62 @@ async function updateMultipleFieldsForRecord(dbName, collectionName, searchField
     return isUpdated;
 }
 
+async function updateMultipleFieldsOfMultipleRecords(dbName, collectionName, filterJson, fieldsToUpdateJson) {
+    let result;
+    let isUpdated = false;
+    try {
+        result = await mongoClient.db(dbName).collection(collectionName).updateMany( filterJson, {$set: (fieldsToUpdateJson)} ); 
+        if(result.modifiedCount > 0){
+            isUpdated = true;
+            logger.info(`Document updated successfully`);
+        }
+        else {
+            logger.info(`Failed to update document`);
+        }
+    } catch(e) {
+        logger.error(e);
+    }
+    return isUpdated;
+}
+
+
+//DELETE Queries
+
+async function deleteRecordById(id, dbName, collectionName) {
+    let isDeleted = false;
+    let document;
+    try {
+        document = await mongoClient.db(dbName).collection(collectionName).deleteOne( {_id: id} ); 
+        if(document.deletedCount > 0){
+            isDeleted = true;
+            logger.info(`Document deleted successfully`);
+        }
+        else {
+            logger.info(`Failed to delete document`);
+        }
+    } catch(e) {
+        logger.error(e);
+    }
+    return isDeleted;
+}
+
 module.exports = {
+    insertRecord,
     insertRecordWithId,
+    insertMultipleRecords,
+    
+    getSingleRecord,
     getRecordById,
+    getAllRecords,
+    getSingleRecordWithMultipleFilters,
+    getRecordsWithOffsetLimit,
+    getRecordsWithProjectionOffsetLimit,
+    getRecordsWithMultipleFilterProjectionOffsetLimit,
+    getAllRecordsWithOffsetLimit,
+
     updateFieldForRecord,
-    updateMultipleFieldsForRecord
+    updateMultipleFieldsForRecord,
+    updateMultipleFieldsOfMultipleRecords,
+
+    deleteRecordById
 }
--- src/services/nft.service.js
+++ src/services/nft.service.js
@@ -1,4 +1,4 @@
-const { insertRecordWithId, getRecordById, updateFieldForRecord, updateMultipleFieldsForRecord } = require("../database/mogodb.operations");
+const { insertRecordWithId, getRecordById, updateFieldForRecord, updateMultipleFieldsForRecord, updateMultipleFieldsOfMultipleRecords, getRecordsWithMultipleFilterProjectionOffsetLimit } = require("../database/mogodb.operations");
 const logger = require("../logger/LoggerConfiguration");
 
 const createNFT = async (reqHeaders, reqBody) => {
@@ -22,13 +22,22 @@ const createNFT = async (reqHeaders, reqBody) => {
 const viewNFT = async(reqHeaders, reqBody) => {
     let response;
     try {
-        let fieldName = "_id";
-        let fieldValue = `${reqBody["nft-contract-address"]}_${reqBody["token-id"]}`;
-        let data = await getRecordById("nftdb", "nft-collection", fieldName, fieldValue);
+        //CASE 1:
+        // let fieldName = "_id";
+        // let fieldValue = `${reqBody["nft-contract-address"]}_${reqBody["token-id"]}`;
+        // let data = await getRecordById("nftdb", "nft-collection", fieldName, fieldValue);
+        let filterJson = reqBody["filter-json"];
+        let projectionJson = reqBody["projection"]
+        let sortedFieldName = reqBody["sorted-field-name"];
+        let offset = reqBody["offset"];
+        let limit = reqBody["limit"];
+        let resultArr = await getRecordsWithMultipleFilterProjectionOffsetLimit("nftdb", "nft-collection", filterJson, 
+        projectionJson, sortedFieldName, offset, limit);
+  
         response = {
             "status-code" : "200",
             "description" : "NFT retrieved successfully",
-            "data": data
+            "data": resultArr
         }
     } catch(err) {
         response = {
@@ -44,9 +53,20 @@ const updateNFT = async(reqHeaders, reqBody) => {
     try {
         let searchFieldName = "_id";
         let searchFieldValue = `${reqBody["nft-contract-address"]}_${reqBody["token-id"]}`;
-        let newFieldName = reqBody["newfieldkey"];
-        let newFieldValue = reqBody["newfieldvalue"];
-        let isUpdated = await updateFieldForRecord("nftdb", "nft-collection", searchFieldName, searchFieldValue, newFieldName, newFieldValue);
+        // CASE 1 : UPDATE SINGLE FIELD OF A DOCUMENT
+        // let newFieldName = reqBody["newfieldkey"];
+        // let newFieldValue = reqBody["newfieldvalue"];
+        // let isUpdated = await updateFieldForRecord("nftdb", "nft-collection", searchFieldName, searchFieldValue, newFieldName, newFieldValue);
+
+        // CASE 2 : UPDATE MULTIPLE FIELDS OF A DOCUMENT
+        // let updateFieldsJson = reqBody["new-data"];
+        // let isUpdated = await updateMultipleFieldsForRecord("nftdb", "nft-collection", searchFieldName, searchFieldValue, updateFieldsJson);
+        
+        // CASE 3 : UPDATE MULTIPLE FIELDS OF MULTIPLE DOCUMENTS based on Filter
+        let filterJson = reqBody["filter-json"];
+        let updateFieldsJson = reqBody["new-data"];
+        let isUpdated = await updateMultipleFieldsOfMultipleRecords("nftdb", "nft-collection", filterJson, updateFieldsJson);
+
         if (isUpdated) {
             response = {
                 "status-code" : "200",
